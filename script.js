13296259f5d2e28ede24006cbd674b0b0add0bd6
poeticAndroid <i@poeticandroid.online>
2026-02-13 13:15:19 +0100

    mvp

---
+++ script.js
@@ -0,0 +1,106 @@
+let api_url = "http://"+location.hostname+":11434"
+let model = ""
+let options = {
+    temperature: 1,
+    //top_k: NaN,
+    //top_p: NaN,
+    //min_p: NaN,
+    //num_ctx: NaN,
+    //num_predict: NaN
+}
+
+let doc = document.getElementById("doc")
+let reader, reqTO
+
+async function init() {
+    let resp = await (await fetch(api_url + "/api/ps")).json()
+    if (!resp.models[0]) resp = await (await fetch(api_url + "/api/tags")).json()
+    model = resp.models[0].name
+    console.log("Model:", model)
+
+    doc.addEventListener("keydown", e => {
+        console.log("e", e)
+        printEnabled = false
+        reader?.cancel()
+        clearTimeout(reqTO)
+        if (!doc.value.trim()) doc.value = sessionStorage.getItem("doc")
+        let txt = doc.value
+        let start = doc.selectionStart
+        let end = doc.selectionEnd
+        if (e.key == "Tab") {
+            e.preventDefault()
+            doc.selectionStart = txt.indexOf(" ", start) + 1
+        }
+        if (e.ctrlKey && e.key == "Enter") {
+            doc.value += "\n\n\nuser: "
+            doc.scrollBy(0, doc.scrollHeight)
+            return
+        }
+        reqTO = setTimeout(async () => {
+            if (txt != doc.value && doc.selectionStart == doc.value.length) {
+                if (getMessages(doc.value).pop().role != "assistant") {
+                    printEnabled = true
+                    print("\n\n\nassistant: ")
+                }
+                await process()
+                sessionStorage.setItem("doc", doc.value)
+            }
+        }, 1024)
+    })
+}
+
+async function process(e) {
+    let payload = {
+        model: model, options: options,
+        messages: getMessages(doc.value)
+        //prompt: doc.value//.slice(0, doc.value.lastIndexOf("\n\n\n")),
+        //suffix: doc.value//.slice(doc.value.lastIndexOf("\n\n\n"))
+    }
+    let resp = await fetch(api_url + "/api/chat", { method: "POST", body: JSON.stringify(payload) })
+    reader = resp.body.pipeThrough(new TextDecoderStream()).getReader()
+    console.log(reader)
+
+    let queue = ""
+    let chunk = { value: "", done: false }
+    printEnabled = true
+    while (!chunk.done) {
+        chunk = await reader.read()
+        queue += chunk.value
+        if (queue.includes("\n")) {
+            let token = JSON.parse(queue.slice(0, queue.indexOf("\n")))
+            console.log(token)
+            queue = queue.slice(queue.indexOf("\n")).trimStart()
+            print(token.message.content)
+        }
+    }
+    print("\n\n\nuser: ")
+}
+
+function getMessages(str) {
+    let messages = []
+    let parafs = str.trim().split("\n\n\n")
+    for (let paraf of parafs) {
+        let words = paraf.split(" ")
+        if (words[0].slice(-1) == ":") {
+            messages.push({
+                role: words[0].slice(0, -1),
+                content: paraf.slice(paraf.indexOf(":") + 1).trim()
+            })
+        } else {
+            if (!messages.length) messages.push({ role: "system", content: "" })
+            messages[messages.length - 1].content += "\n\n\n" + paraf
+        }
+    }
+    return messages
+}
+
+let printEnabled
+function print(str) {
+    if (!printEnabled) return
+    let start = doc.selectionStart
+    doc.value += str
+    // doc.scrollBy(0, 1024)
+    doc.selectionStart = start
+}
+
+init()
